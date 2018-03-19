2b8ac9cae4381f82a532d3350721d4611cb77270
John Labovitz <johnl@johnlabovitz.com>
2018-03-19 11:19:31 -0400

    Move add/import logic from command into Profile.

--- lib/feeder/commands/add.rb
+++ lib/feeder/commands/add.rb
@@ -8,40 +8,9 @@ module Feeder
 
       def run(args)
         raise Error, "No profile specified" unless @profile
-        feed_link = args.shift or raise "No feed URI specified"
+        uri = args.shift or raise Error, "No feed URI specified"
         path = args.shift
-        feed_link = URI.parse(feed_link)
-        response = Feeder.get(feed_link)
-        raise Error, "Failed to get URI #{feed_link}: #{response.status}" unless response.success?
-        begin
-          jira_feed = Feedjira::Feed.parse(response.body)
-        rescue Feedjira::NoParserAvailable => e
-          feeds = find_feeds(response.body)
-          raise Error, "No alternate links in URI: #{feed_link}" if feeds.empty?
-          feeds.each do |uri, type, title|
-            puts "%s (%s): %s" % [uri, type, title]
-          end
-          return
-        end
-        #FIXME: save feed
-        feed = Feed.new(
-          id: [path, Feeder.uri_to_key(feed_link)].compact.join('/'),
-          feed_link: feed_link,
-          profile: @profile)
-        feed.save
-        ;;warn "saved new feed to #{feed.info_file}"
-      end
-
-      def find_feeds(html_str)
-        html = Nokogiri::HTML::Document.parse(html_str)
-        html.xpath('//link[@rel="alternate"]').map do |link_elem|
-          [
-            # feed_link + URI.parse(link_elem['href']),
-            link_elem['href'],
-            link_elem['type'],
-            link_elem['title'],
-          ]
-        end
+        @profile.add_feed(uri: uri, path: path)
       end
 
     end
--- lib/feeder/profile.rb
+++ lib/feeder/profile.rb
@@ -61,6 +61,67 @@ module Feeder
       threads.map(&:join)
     end
 
+    def import_plist(plist_file)
+      io = IO.popen(['plutil', '-convert', 'xml1', '-o', '-', plist_file], 'r')
+      plist = Nokogiri::PList(io)
+      recurse_plist(plist) do |item, path|
+        key = Feeder.uri_to_key(item['rss'])
+        puts "importing %-60s => %s" % [item['rss'], key]
+        feed = Feed.new(
+          id: [path, key].join('/'),
+          feed_link: item['rss'],
+          profile: self)
+        raise "Feed exists: #{feed.info_file}" if feed.info_file.exist?
+        feed.save
+        ;;warn "saved new feed to #{feed.info_file}"
+      end
+    end
+
+    def recurse_plist(items, path=[], &block)
+      items.each do |item|
+        if item['isContainer']
+          recurse_plist(item['childrenArray'], path + [item['name']], &block)
+        else
+          yield(item, path)
+        end
+      end
+    end
+
+    def add_feed(uri:, path: nil)
+      uri = URI.parse(uri)
+      response = Feeder.get(uri)
+      raise Error, "Failed to get URI #{uri}: #{response.status}" unless response.success?
+      begin
+        jira_feed = Feedjira::Feed.parse(response.body)
+      rescue Feedjira::NoParserAvailable => e
+        feeds = find_feeds(response.body)
+        raise Error, "No alternate links in URI: #{uri}" if feeds.empty?
+        feeds.each do |uri, type, title|
+          puts "%s (%s): %s" % [uri, type, title]
+        end
+        return
+      end
+      #FIXME: save feed
+      feed = Feed.new(
+        id: [path, Feeder.uri_to_key(uri)].compact.join('/'),
+        feed_link: uri,
+        profile: self)
+      feed.save
+      ;;warn "saved new feed to #{feed.info_file}"
+    end
+
+    def find_feeds(html_str)
+      html = Nokogiri::HTML::Document.parse(html_str)
+      html.xpath('//link[@rel="alternate"]').map do |link_elem|
+        [
+          # feed_link + URI.parse(link_elem['href']),
+          link_elem['href'],
+          link_elem['type'],
+          link_elem['title'],
+        ]
+      end
+    end
+
   end
 
 end
