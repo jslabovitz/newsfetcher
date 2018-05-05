45642b12d125f67109c503f3904aba5ab860f703
John Labovitz <johnl@johnlabovitz.com>
2018-05-05 07:14:08 -0400

    Ensure hostname is compared case-insensitively.

--- lib/newsfetcher.rb
+++ lib/newsfetcher.rb
@@ -44,7 +44,7 @@ module NewsFetcher
   def self.uri_to_key(uri)
     uri = URI.parse(uri)  unless uri.kind_of?(URI)
     [
-      uri.host.to_s.sub(/^(www|ssl|en|feeds|blogs?|news).*?\./i, '').sub(/\.(com|org|net|info|edu|co\.uk)$/, ''),
+      uri.host.to_s.sub(/^(www|ssl|en|feeds|blogs?|news).*?\./i, '').sub(/\.(com|org|net|info|edu|co\.uk)$/i, ''),
       uri.path.to_s.gsub(/\b(feed|atom|rss2|xml)\b/i, ''),
       uri.query.to_s.gsub(/(format|feed|type|q)=(atom|rss2?|xml|rss\.xml)/i, ''),
     ].reject { |s| s.empty? }.join('-').
