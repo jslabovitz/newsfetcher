fb6d757f041ebb2295fc02a7e8fe2f671dfd6b00
John Labovitz <johnl@johnlabovitz.com>
2018-05-06 10:03:03 -0400

    Make updates happen in parallel.

--- lib/newsfetcher/commands/update.rb
+++ lib/newsfetcher/commands/update.rb
@@ -4,16 +4,30 @@ module NewsFetcher
 
     class Update < Command
 
-      register_command 'update'
+      register_command 'update',
+        limit: 100
+
+      attr_accessor :limit
 
       def run(args)
+        threads = []
         @profile.subscriptions(args).each do |subscription|
-          begin
-            subscription.update
-          rescue Error => e
-            warn "#{subscription.id}: #{e}"
+          if threads.length >= @limit
+            # ;;warn "waiting for #{threads.length} threads to finish"
+            threads.map(&:join)
+            threads = []
+          end
+          threads << Thread.new do
+            # ;;warn "started thread for #{subscription.id}"
+            begin
+              subscription.update
+            rescue Error => e
+              warn "#{subscription.id}: #{e}"
+            end
           end
         end
+        # ;;warn "waiting for last #{threads.length} threads to finish"
+        threads.map(&:join)
       end
 
     end
