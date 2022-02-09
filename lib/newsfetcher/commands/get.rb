f884174ec7bfc4c2d0adcbb543df9ecb47aaffa1
John Labovitz <johnl@johnlabovitz.com>
2022-02-09 11:28:50 -0800

    Add 'get' command to fetch & show feed.

---
+++ lib/newsfetcher/commands/get.rb
@@ -0,0 +1,30 @@
+module NewsFetcher
+
+  module Commands
+
+    class Get < Command
+
+      def run(args)
+        super
+        args.each do |uri|
+          feed = Feed.get(uri)
+          puts "URI: #{feed.uri}"
+          puts "Title: #{feed.title.inspect}"
+          puts "Items:"
+          feed.items.each do |item|
+            puts "\t" + "ID: #{item.id}"
+            puts "\t" + "URI: #{item.uri}"
+            puts "\t" + "Date: #{item.date}"
+            puts "\t" + "Author: #{item.author}"
+            puts "\t" + "Title: #{item.title}"
+            puts
+          end
+          puts
+        end
+      end
+
+    end
+
+  end
+
+end
