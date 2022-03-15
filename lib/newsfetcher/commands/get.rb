9ca66eb6df0ca878a9deb271996453a80fdcf329
John Labovitz <johnl@johnlabovitz.com>
2022-03-15 09:22:19 +0100

    Fix 'get' command.

--- lib/newsfetcher/commands/get.rb
+++ lib/newsfetcher/commands/get.rb
@@ -7,17 +7,14 @@ module NewsFetcher
       def run(args)
         super
         args.each do |uri|
-          resource = Resource.get(uri)
-          subscription = Subscription.new_from_resource(resource)
-          puts "URI: #{subscription.uri}"
-          puts "Title: #{subscription.title.inspect}"
-          puts "Items:"
+          uri = Addressable::URI.parse(uri)
+          subscription = Subscription.new(
+            id: Subscription.uri_to_id(uri),
+            config: Config.new(uri: uri))
+          subscription.get
+          subscription.print(format: :list)
           subscription.items.each do |item|
-            puts "\t" + "ID: #{item.id}"
-            puts "\t" + "URI: #{item.uri}"
-            puts "\t" + "Published: #{item.published}"
-            puts "\t" + "Author: #{item.author}"
-            puts "\t" + "Title: #{item.title}"
+            item.show
             puts
           end
           puts
