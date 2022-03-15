6e488a245c96109cb6b6a99bf0d0aceccbd3963a
John Labovitz <johnl@johnlabovitz.com>
2022-03-15 08:33:35 +0100

    Fix 'add' command.

--- lib/newsfetcher/commands/add.rb
+++ lib/newsfetcher/commands/add.rb
@@ -9,8 +9,11 @@ module NewsFetcher
         uri, path = *args
         raise Error, "No URI specified" unless uri
         uri = Addressable::URI.parse(uri)
-        subscription = Subscription.new(uri, path: path)
+        subscription = Subscription.new(
+          id: Subscription.name_to_id(uri, path: path),
+          config: Config.new(uri: uri))
         @profile.add_subscription(subscription)
+        warn "Added subscription: #{subscription.id}"
       end
 
     end
