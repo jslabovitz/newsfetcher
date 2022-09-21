7857c98f47df41ed3ce138ce6e60e32822883b03
John Labovitz <johnl@johnlabovitz.com>
2022-09-21 10:30:14 +0200

    Convert log level to symbol when parsing.

--- lib/newsfetcher/command.rb
+++ lib/newsfetcher/command.rb
@@ -7,7 +7,7 @@ module NewsFetcher
       dir = Path.new(@dir || DefaultProfileDir)
       config_file = dir / ConfigFileName
       config = config_file.exist? ? BaseConfig.load(config_file) : BaseConfig.make
-      config.log_level = @log_level if @log_level
+      config.log_level = @log_level.downcase.to_sym if @log_level
       config.max_threads = @max_threads if @max_threads
       @profile = Profile.new(dir: dir, config: config) or raise Error, "Profile not loaded"
     end
