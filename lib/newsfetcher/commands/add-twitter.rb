e70f74c61399ff0e500e5340758821e141ed951f
John Labovitz <johnl@johnlabovitz.com>
2022-12-16 10:16:12 +0000

    Rework AddTwitter command.

--- lib/newsfetcher/commands/add-twitter.rb
+++ lib/newsfetcher/commands/add-twitter.rb
@@ -6,12 +6,11 @@ module NewsFetcher
 
       def run(args)
         super
-        file, path = *args
-        file = Path.new(file)
-        config = Config.new(twitter: JSON.parse(file.read))
-        subscription = Subscription::Twitter.new(
-          id: 'twitter',
-          config: config)
+        id, config_file = *args
+        config_file = Path.new(config_file)
+        config = Config.new(twitter: JSON.parse(config_file.read))
+        subscription = Subscriptions::Twitter::Subscription.new(id: id, config: config)
+# ;;subscription.show_lists; exit
         @profile.add_subscription(subscription)
         warn "Added subscription: #{subscription.id}"
       end
