db285166b33976913a1847f01c7d9b39640f33e0
John Labovitz <johnl@johnlabovitz.com>
2023-12-04 17:50:34 -0500

    Make sure max_threads is converted to Integer.

--- lib/newsfetcher/command.rb
+++ lib/newsfetcher/command.rb
@@ -7,7 +7,7 @@ module NewsFetcher
       config_file = dir / ConfigFileName
       config = config_file.exist? ? BaseConfig.load(config_file) : BaseConfig.make
       config.log_level = @log_level.downcase.to_sym if @log_level
-      config.max_threads = @max_threads if @max_threads
+      config.max_threads = @max_threads&.to_i if @max_threads
       @profile = Profile.new(dir: dir, config: config) or raise Error, "Profile not loaded"
     end
 
